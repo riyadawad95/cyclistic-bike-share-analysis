a9dec7fc0cf5457a7d20d7390bb7c95a627282e3
riyadawad95 <riyadawad95@gmail.com>
2026-06-15 13:41:38 -0400

    Update 01_import_and_prepare.R

--- scripts/01_import_and_prepare.R
+++ scripts/01_import_and_prepare.R
@@ -1,1 +1,15 @@
+install.packages("tidyverse")
+install.packages("lubridate")
+library(tidyverse)
+library(lubridate)
+list.files("data_raw")
+trip_files <- list.files(
+  path = "data_raw",
+  pattern = "*.csv",
+  full.names = TRUE
+)
 
+length(trip_files)
+jan2025 <- read_csv("data_raw/202501-divvy-tripdata.csv")
+
+glimpse(jan2025)
