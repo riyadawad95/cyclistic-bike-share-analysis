685332a54c0dbc7386012b3a1d1b5f23da3d037a
riyadawad95 <riyadawad95@gmail.com>
2026-06-15 13:47:19 -0400

    Update 01_import_and_prepare.R

--- scripts/01_import_and_prepare.R
+++ scripts/01_import_and_prepare.R
@@ -1,8 +1,10 @@
-install.packages("tidyverse")
-install.packages("lubridate")
+# Cyclistic Bike Share Capstone
+# Phase: Prepare
+# Purpose: Import and validate source data
+
 library(tidyverse)
 library(lubridate)
-list.files("data_raw")
+
 trip_files <- list.files(
   path = "data_raw",
   pattern = "*.csv",
@@ -10,6 +12,8 @@ trip_files <- list.files(
 )
 
 length(trip_files)
+
 jan2025 <- read_csv("data_raw/202501-divvy-tripdata.csv")
 
 glimpse(jan2025)
+colnames(jan2025)
